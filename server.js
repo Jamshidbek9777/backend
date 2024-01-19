07eff4c86c8e7a7f37d639d20670aa4400365def
Jamshidbek <jamshidbekrasulovuz@gmail.com>
2024-01-19 20:00:55 +0500

    commit

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = require("./index");
 // const cors = require("cors");
 
 app.use((req, res, next) => {
-     res.header("Access-Control-Allow-Origin", "http://localhost:300");
+     res.header("Access-Control-Allow-Origin", "http://localhost:3000");
      res.header(
           "Access-Control-Allow-Methods",
           "GET,HEAD,PUT,PATCH,POST,DELETE"
