a15237edae9ac285787172117d5d71816e5f6379
Jamshidbek <jamshidbekrasulovuz@gmail.com>
2024-01-19 16:59:17 +0500

    commit

--- Routes/moviesRouter.js
+++ Routes/moviesRouter.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const moviesController = require("../Controller/moviesController");
+const cors = require("cors");
+app.use(
+     cors({
+          origin: "https://127.0.0.1:3000",
+     })
+);
 const router = express.Router();
 router
-    .route("/")
-    .get(moviesController.getAllMovies)
-    .post(moviesController.createMovie);
+     .route("/")
+     .get(moviesController.getAllMovies)
+     .post(moviesController.createMovie);
 router
-    .route("/:id")
-    .get(moviesController.getMovieWithId)
-    .patch(moviesController.updateMovie)
-    .delete(moviesController.deleteMovie);
+     .route("/:id")
+     .get(moviesController.getMovieWithId)
+     .patch(moviesController.updateMovie)
+     .delete(moviesController.deleteMovie);
 
 module.exports = router;
